eeed8ef5316c7beb5200945b3e9116b95cf8a9d0
Luciano Zapata <lzapata@ater.gob.ar>
2023-04-08 09:24:03 -0300

    add AxiosAdapter in seedServices

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,14 +3,14 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { FetchAdapter } from 'src/common/adapters/fetch.adapter';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-    private http: FetchAdapter,
+    private http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
